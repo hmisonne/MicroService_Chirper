9311210e72cb77dae63ed074bf03bacf25456e02
hmisonne <helene.misonne@gmail.com>
2020-07-03 13:27:53 -0700

    fix: Added user to sample database to match with author of tweet in DB

--- src/utils/_DATA.js
+++ src/utils/_DATA.js
@@ -18,6 +18,12 @@ let users = {
     name: "Dan Abramov",
     avatarURL: "https://tylermcginnis.com/would-you-rather/dan.jpg",
     tweets: ['5w6k1n34dkp1x29cuzn2zn', 'czpa59mg577x1oo45cup0d', 'omdbjl68fxact38hk7ypy6', '3km0v4hf1ps92ajf4z2ytg', 'njv20mq7jsxa6bgsqc97', 'sfljgka8pfddbcer8nuxv', 'r0xu2v1qrxa6ygtvf2rkjw'],
+  },
+  Author1: {
+    id: "Author1",
+    name: "Author1",
+    avatarURL: "https://tylermcginnis.com/would-you-rather/dan.jpg",
+    tweets: ['5w6k1n34dkp1x29cuzn2zn', 'czpa59mg577x1oo45cup0d', 'omdbjl68fxact38hk7ypy6', '3km0v4hf1ps92ajf4z2ytg', 'njv20mq7jsxa6bgsqc97', 'sfljgka8pfddbcer8nuxv', 'r0xu2v1qrxa6ygtvf2rkjw'],
   }
 }
 
