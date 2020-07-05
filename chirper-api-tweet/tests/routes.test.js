f82a0dd66cebc18c1200c874b887f769a05a0bbe
hmisonne <helene.misonne@gmail.com>
2020-07-05 16:32:29 -0700

    test: Added new tests

--- chirper-api-tweet/tests/routes.test.js
+++ chirper-api-tweet/tests/routes.test.js
@@ -1,6 +1,8 @@
 const request = require('supertest')
 const app = require('../app.js')
-describe('User API', () => {
+const db = require('../controllers/v0/tweet/models/index.js')
+
+describe('Tweet API', () => {
     it('should show all tweets', async () => {
         const res = await request(app).get('/api/v0/tweet')
         expect(res.statusCode).toEqual(200)
@@ -12,6 +14,12 @@ describe('User API', () => {
         expect(res.statusCode).toEqual(200)
         expect(res.body).toHaveProperty('tweet')
     }),
+    it('should show an error message if tweet id does not exist', async () => {
+        const res = await request(app)
+        	.get('/api/v0/tweet/INVALIDID')
+        expect(res.statusCode).toEqual(500)
+        expect(res.body).toHaveProperty('error')
+    }),
     it('should update a tweet', async () => {
         const res = await request(app)
             .patch('/api/v0/tweet/46ab8e66-f3a4-48ae-a9e4-040ca0b29186')
@@ -19,6 +27,15 @@ describe('User API', () => {
                 text: 'Updated'
             })
         expect(res.statusCode).toEqual(200)
+    }),
+    it('should show an error message if tweet id does not exist', async () => {
+       const res = await request(app)
+            .patch('/api/v0/tweet/INVALIDID')
+            .send({
+                text: 'Updated'
+            })
+        expect(res.statusCode).toEqual(500)
+        expect(res.body).toHaveProperty('error')
     }),
     it('should create a new tweet', async () => {
         const res = await request(app)
@@ -30,10 +47,25 @@ describe('User API', () => {
         expect(res.statusCode).toEqual(201)
         expect(res.body).toHaveProperty('tweet')
     }),
+    it('should show an error message if a parameter is missing', async () => {
+        const res = await request(app)
+        	.post('/api/v0/tweet')
+        	.send({
+        		text: 'My test Tweet',
+        	})
+        expect(res.statusCode).toEqual(400)
+        expect(res.body.success).toEqual(false)
+        expect(res.body.error).toEqual("Missing text and/or author")
+    }),
     it('should delete a tweet', async () => {
         const res = await request(app)
         	.del('/api/v0/tweet/46ab8e66-f3a4-48ae-a9e4-040ca0b29186')
         expect(res.statusCode).toEqual(200)
-    })
-    
+    }),
+    afterAll(async done => {
+	  // Closing the DB connection allows Jest to exit successfully.
+	  db.sequelize.close();
+	  done();
+	});    
 })
+
