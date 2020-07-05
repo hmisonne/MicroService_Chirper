1409a4b4b67641b72eca98c85a6a92013fbb2f81
hmisonne <helene.misonne@gmail.com>
2020-07-05 08:51:18 -0700

    refactor: added try catch statements and await keyword to replace then

--- chirper-api-tweet/controllers/v0/tweet/routes/tweet.router.js
+++ chirper-api-tweet/controllers/v0/tweet/routes/tweet.router.js
@@ -2,55 +2,70 @@ const models = require('../models')
 
 
 
-exports.submit_tweet = function(req, res, next) {	
-  return models.TweetItem.create({
-  	text: req.body.text,
-  	author: req.body.author,
-  	replyingTo: req.body.replyingTo
-  }).then(tweet => {
-  	res.status(201).send({msg: "Success", tweet})
-  })
+exports.submit_tweet = async(req, res, next) => {	
+  try {
+    const tweet = await models.TweetItem.create({
+      text: req.body.text,
+      author: req.body.author,
+      replyingTo: req.body.replyingTo
+    })
+    return res.status(201).send({msg: "Success", tweet})
+  } catch (error) {
+    return res.status(500).json({ error: error.message})
+  }
   
 }
 
-exports.show_tweets = function(req, res, next) {
-  return models.TweetItem.findAll().then(tweets => {
-  	res.status(200).send(tweets)
-  })
-  
-}
-
-
-exports.show_tweet = function(req, res, next) {
-  return models.TweetItem.findOne({
-  	where: {
-  		id: req.params.tweet_id
-  	}
-  }).then(tweet => {
-  	res.status(200).send({msg: "Success", tweet})
-  })
+exports.show_tweets = async(req, res, next) => {
+  try {
+    const tweets = await models.TweetItem.findAll()
+    return res.status(200).send(tweets)
+  } catch (error) {
+    return res.status(500).json({ error: error.message})
+  }
   
 }
 
 
-exports.edit_tweet = function(req, res, next) {
-	return models.TweetItem.update({
-	  	text: req.body.tweet_text
-	  }, {
-	  	where: {
-	  		id: req.params.tweet_id
-	  	}
-	  }).then(result => {
-	  	res.status(200).send({msg: "Success"});
-	  })
+exports.show_tweet = async(req, res, next) => {
+  try {
+    const tweet = await models.TweetItem.findOne({
+      where: {
+        id: req.params.tweet_id
+      }
+    })
+    return res.status(200).send(tweet)
+  } catch (error) {
+    return res.status(500).json({ error: error.message})
+  }
+  
 }
 
-exports.delete_tweet = function(req, res, next) {
-	return models.TweetItem.destroy({
-	  	where: {
-	  		id: req.params.tweet_id
-	  	}
-	  }).then(result => {
-	  	res.status(200).send({msg: "Success"});
-	  })
+
+exports.edit_tweet = async(req, res, next) => {
+	 try {
+    await models.TweetItem.update({
+      text: req.body.tweet_text
+      }, {
+      where: {
+        id: req.params.tweet_id
+      }
+    })
+    return res.status(200).send({msg: "Success"})
+  } catch (error) {
+    return res.status(500).json({ error: error.message})
+  }
+}
+
+exports.delete_tweet = async(req, res, next) => {
+	try {
+    await models.TweetItem.destroy({
+      where: {
+        id: req.params.tweet_id
+      }
+    })
+    return res.status(200).send({msg: "Success"})
+  } catch (error) {
+    return res.status(500).json({ error: error.message})
+  }
 }
