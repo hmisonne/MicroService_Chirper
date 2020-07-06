46bb40b744f245d40c03f96c6ae00f73ce14c7a4
hmisonne <helene.misonne@gmail.com>
2020-07-06 13:53:00 -0700

    feat: Added auth0 config

--- src/config.js
+++ src/config.js
@@ -1,1 +1,7 @@
 export const apiEndpoint = `http://localhost:8080/api/v0/tweet`
+
+export const authConfig = {
+  domain: 'fsnd-hm.auth0.com',            // Auth0 domain
+  clientId: '1utMzxRA3Wb0CX3tXqQ1aEN4nWCrxx61',          // Auth0 client id
+  callbackUrl: 'http://localhost:3000/callback'
+}
