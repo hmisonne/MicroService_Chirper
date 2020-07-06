ce5adb2c44343bf22708cdd84788f68e6d17742d
hmisonne <helene.misonne@gmail.com>
2020-07-06 16:50:12 -0700

    feat: Added requireAuth middleware to tweetRouter

--- chirper-api-tweet/controllers/v0/index.router.js
+++ chirper-api-tweet/controllers/v0/index.router.js
@@ -1,10 +1,56 @@
 const express = require('express');
 const router = express.Router();
+const jwt = require('jsonwebtoken');
+const env = process.env.NODE_ENV || 'development';
+const path = require('path');
+const config = require(path.join(__dirname, '../../', 'config/database.json'))[env];
+const Axios = require('axios')
 
 const tweetRouter = require('./tweet/routes/tweet.router')
+const jwksUrl = 'https://fsnd-hm.auth0.com/.well-known/jwks.json'
 
+async function requireAuth(req, res, next) {
+    
+    if (!req.headers || !req.headers.authorization) {
+      return res.status(401).send({message: 'No authorization headers.'});
+    }
+    try {
+        const token = await verifyToken(req.headers.authorization)
+        console.log('to', token)
+        return next();
+    } catch(e) {
+        return res.status(500).send({auth: false, message: 'Failed to authenticate.'});
+    }
+    
+  }
 
-router.post('/',tweetRouter.submit_tweet);
+function getToken(authHeader) {
+    if (!authHeader) throw new Error('No authentication header')
+
+    if (!authHeader.toLowerCase().startsWith('bearer '))
+        throw new Error('Invalid authentication header')
+
+    const split = authHeader.split(' ')
+    const token = split[1]
+
+    return token
+}
+
+async function verifyToken(authHeader) {
+    const token = getToken(authHeader)
+    console.log('to1', token)
+    let cert;
+    try {
+        const response = await Axios.get(jwksUrl);
+        const pemData = response['data']['keys'][0]['x5c'][0];
+        cert = `-----BEGIN CERTIFICATE-----\n${pemData}\n-----END CERTIFICATE-----`;
+    } catch (err) {
+        console.log(err);
+    }
+    return jwt.verify(token, cert, { algorithms: ['RS256'] }).sub
+}
+
+router.post('/',requireAuth, tweetRouter.submit_tweet);
 router.get('/',tweetRouter.show_tweets);
 router.get('/:tweet_id',tweetRouter.show_tweet);
 
