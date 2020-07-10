86d9c718804c66e05e91e90dacc380607ea43646
hmisonne <helene.misonne@gmail.com>
2020-07-10 06:58:24 -0700

    style: Added react semantic ui

--- chiper-frontend/src/index.js
+++ chiper-frontend/src/index.js
@@ -1,8 +1,9 @@
-import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import { makeAuthRouting } from './routing';
+import 'semantic-ui-css/semantic.min.css'
 
 ReactDOM.render( makeAuthRouting(), 
 	document.getElementById('root')
 	);
+
